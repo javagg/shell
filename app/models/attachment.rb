fcd07a39fd7290e507ff41ce9ea1501934affa31
javagg <lu.lee05@gmail.com>
2010-11-15 00:34:46 +0800

    bugfix

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -19,7 +19,9 @@
 class Attachment < ActiveRecord::Base
   has_attached_file :data, :path => ":rails_root/data/attachments/:id/:style/:basename.:extension"
 
-  validates_attachment_size :data, :less_than => eval(Settings.upload_limit), :message => "Your attachment file was too large. Attachments must be #{eval(Settings.upload_limit)} or less."
+  validates_attachment_size :data, :less_than => 1.megabytes
+#  eval(Settings.upload_limit),
+#  :message => "Your attachment file was too large. Attachments must be #{eval(Settings.upload_limit)} or less."
 
   belongs_to :attachable, :polymorphic => true
 end
