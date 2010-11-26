690f66262dbdcb357ad4aa979d0492916672291b
javagg <lu.lee05@gmail.com>
2010-11-27 01:54:11 +0800

    annoying deprecation

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -10,6 +10,7 @@ class Contract <  ActiveRecord::Base
   has_many :payments, :dependent => :destroy
   has_many :payment_periods, :dependent => :destroy
   has_many :reminding_periods, :as => :reminder, :dependent => :destroy
+
   has_many :payment_remindings, :as => :reminder, :dependent => :destroy
   has_many :payment_remindees, :through => :payment_remindings, :source => 'user'
 
