ac231bb5ce70fb819660e205e2f967f9c598dad6
javagg <lu.lee05@gmail.com>
2010-11-28 18:12:24 +0800

    delete reminding period

--- app/controllers/reminding_periods_controller.rb
+++
@@ -1,6 +0,0 @@
-class RemindingPeriodsController < ApplicationController
-  active_scaffold :reminding_periods do |config|
-    config.columns = [ :start_date, :end_date, :num_remindings ]
-    config.subform.layout = :vertical
-  end
-end
--- test/unit/helpers/reminding_periods_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class RemindingPeriodsHelperTest < ActionView::TestCase
-end
